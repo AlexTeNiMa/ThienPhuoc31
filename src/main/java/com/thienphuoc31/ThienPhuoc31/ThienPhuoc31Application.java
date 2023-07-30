750a25d89a022ace355bc193617898b6723b2a04
AlexTeNiMa <phuocb1906552@student.ctu.edu.vn>
2023-07-30 09:30:53 +0700

    Initial commit

---
+++ src/main/java/com/thienphuoc31/ThienPhuoc31/ThienPhuoc31Application.java
@@ -0,0 +1,13 @@
+package com.thienphuoc31.ThienPhuoc31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThienPhuoc31Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThienPhuoc31Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/thienphuoc31/ThienPhuoc31/ThienPhuoc31ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thienphuoc31.ThienPhuoc31;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThienPhuoc31ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
